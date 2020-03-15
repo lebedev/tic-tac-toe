f4443c20c290a9e85302ff66b80819638b41290c
Serge Lebedev <angly.cat@gmail.com>
2020-03-15 16:54:37 +0600

    Implement Game screen

--- front/src/reducers/game.js
+++ front/src/reducers/game.js
@@ -1,7 +1,12 @@
+import { FETCH_GAME_SUCCESS } from '../actions/constants';
+
 const initialState = null;
 
 export default (state = initialState, action) => {
     switch (action.type) {
+        case FETCH_GAME_SUCCESS:
+            return action.payload;
+
         default:
             return state;
     }
